1eaad3d380a57a82e41544c28d72af0772025fab
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2023-10-11 17:47:43 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+        if (name == String.Empty)
+            name = "No name";
+        Console.WriteLine("Welcome {0} to dotnet world", name);
+    }
+}
